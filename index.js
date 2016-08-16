916cf5973b27532aca25e344e15c6555aaa13fde
Taylor Vance <tvance989@gmail.com>
2016-08-16 14:32:39 -0500

    Get rid of angular for now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/game.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
