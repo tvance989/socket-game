31f87f1d1ede67898e415d502e50755c6f5750de
Taylor Vance <tvance989@gmail.com>
2016-08-16 15:16:10 -0500

    Button click game

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ var io = require('socket.io')(http);
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/game.html');
 });
 
+var num_clicks = 0;
+var clicks = {};
+
 io.on('connection', function(socket){
-	socket.broadcast.emit('chat message', 'user connected: '+socket.id);
+	socket.emit('set button', num_clicks);
+	clicks[socket.id] = 0;
 	console.log('a user connected');
 
+	socket.on('button click', function(){
+		num_clicks++;
+		io.emit('set button', num_clicks);
+
+		clicks[socket.id]++;
+		io.emit('debug', clicks);
+
+		console.log('button clicked. '+num_clicks+' total.');
+	});
+
 	socket.on('disconnect', function(){
-		socket.broadcast.emit('chat message', 'user disconnected: '+socket.id);
 		console.log('user disconnected');
-	});
-
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-		console.log('message: '+msg+' -' + (socket.username || socket.id));
-	});
-
-	socket.on('set username', function(username){
-		socket.username = username;
-		console.log('new name: '+socket.id+' => '+username);
+		delete clicks[socket.id];
 	});
 });
 
