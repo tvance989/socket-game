6bf4471026b2ecc311e042691e003b819f57dca1
Taylor Vance <tvance989@gmail.com>
2016-08-16 11:36:01 -0500

    Todo app angular test

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/game.html');
 });
 
 io.on('connection', function(socket){
